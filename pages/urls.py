d0365c4758fb53d234ea50e743ec5e46ca9c518d
Knig2 <gominaeloge@gmail.com>
2026-01-08 08:25:10 +0000

    page visiteur de l'application pages

---
+++ pages/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("",views.home , name="home" ),
+    path("events/",views.events,name="events"),
+    path("poste/",views.poste,name="poste"),
+]
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def home(request):
+    return render(request, 'index.html')
+
+def events(request):
+    return render(request, 'pages/events.html')
+
+def poste(request):
+    return render(request, 'pages/poste.html')
